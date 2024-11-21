b482c3ae1ad9f8ac46b22448e6e7e6e365331c36
Irina Kalmykova <1006-2017@mail.ru>
2024-11-21 23:28:01 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10_500;
+
+        int miles = amount / 20;
+        System.out.println("Бонусные мили: " + miles);
+    }
+}
